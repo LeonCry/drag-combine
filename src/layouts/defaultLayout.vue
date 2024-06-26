c7529eaa71f4fded5ad5e0203a5f1288c0cf21a0
seaci <73209933+LeonCry@users.noreply.github.com>
2024-06-26 10:29:54 +0800

    Initial commit

---
+++ src/layouts/defaultLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="flex h-svh w-svw overflow-auto">
+    <aside>AsideBar</aside>
+    <section class="relative flex min-w-[900px] flex-1 flex-col">
+      <header>Header</header>
+      <RouterView class="flex-1 overflow-auto" />
+    </section>
+  </main>
+</template>
+<script setup lang="ts"></script>
+<style scoped lang="scss"></style>
